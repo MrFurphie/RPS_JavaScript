78034fcfd0959d50bb5d9526a75231a9b0cded6c
Henry Felsman <felsmahp@clarkson.edu>
2021-10-28 13:15:29 -0400

    Update page.js

--- js/page.js
+++ js/page.js
@@ -1,1 +1,16 @@
 
+function play() {
+  let cPick = "?";
+  let msg = Math.floor(Math.random() * 3 + 1);
+  function set() {
+  if (msg == 1){
+    let cPick = "rock";
+  } else if (msg == 2) {
+    let cPick = "paper";
+  } else if (msg == 3){
+    let cPick = "scissors";
+  }
+};
+  set();
+  document.getElementById('result').innerHTML = cPick;
+}
