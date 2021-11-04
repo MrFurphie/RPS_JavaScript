b2d44bf16d0c6eb57b3fca8d7f678f58b920e728
Henry Felsman <felsmahp@clarkson.edu>
2021-11-04 11:09:34 -0400

    Update page.js

--- js/page.js
+++ js/page.js
@@ -1,16 +1,79 @@
-
-function play() {
-  let cPick = "?";
-  let msg = Math.floor(Math.random() * 3 + 1);
-  function set() {
-  if (msg == 1){
-    let cPick = "rock";
-  } else if (msg == 2) {
-    let cPick = "paper";
-  } else if (msg == 3){
-    let cPick = "scissors";
-  }
+function play(choice) {
+let pChoice = choice;
+let oChoice = Math.floor(Math.random() * 3 + 1);
+document.getElementById('o-img').src = "";
+document.getElementById('result').innerHTML = "?";
+let wins = sessionStorage.getItem("winCount");
+let ties = sessionStorage.getItem("tieCount");
+let losses = sessionStorage.getItem("lossCount");
+if (wins == null) {
+  wins = 0;
+} else {
+  wins = wins;
 };
-  set();
-  document.getElementById('result').innerHTML = cPick;
+if (ties == null) {
+  ties = 0;
+} else {
+  ties = ties;
+};
+if (losses == null) {
+  losses = 0;
+} else {
+  losses = losses;
+};
+setTimeout(() => {
+  if (oChoice === 1 && pChoice === "rock") {
+    document.getElementById('o-img').src = "svg/rock.svg";
+    document.getElementById('result').innerHTML = "Tie.";
+    ++ties;
+  } else if (oChoice === 1 && pChoice === "scissors")  {
+    document.getElementById('o-img').src = "svg/rock.svg";
+    document.getElementById('result').innerHTML = "You lose.";
+    ++losses;
+  } else if (oChoice === 1 && pChoice === "paper") {
+    document.getElementById('o-img').src = "svg/rock.svg";
+    document.getElementById('result').innerHTML = "You win.";
+    ++wins;
+  } else if (oChoice === 2 && pChoice === "rock")  {
+    document.getElementById('o-img').src = "svg/paper.svg";
+    document.getElementById('result').innerHTML = "You lose.";
+    ++losses;
+  } else if (oChoice === 2 && pChoice === "paper")  {
+    document.getElementById('o-img').src = "svg/paper.svg";
+    document.getElementById('result').innerHTML = "Tie.";
+    ++ties;
+  } else if (oChoice === 2 && pChoice === "scissors")  {
+    document.getElementById('o-img').src = "svg/paper.svg";
+    document.getElementById('result').innerHTML = "You win.";
+    ++wins;
+  } else if (oChoice === 3 && pChoice === "rock")  {
+    document.getElementById('o-img').src = "svg/scissors.svg";
+    document.getElementById('result').innerHTML = "You win.";
+    ++wins;
+  } else if (oChoice === 3 && pChoice === "paper")  {
+    document.getElementById('o-img').src = "svg/scissors.svg";
+    document.getElementById('result').innerHTML = "You lose.";
+    ++losses;
+  } else {
+    document.getElementById('o-img').src = "svg/scissors.svg";
+    document.getElementById('result').innerHTML = "Tie.";
+    ++ties;
+  }
+  sessionStorage.setItem("winCount", wins);
+  sessionStorage.setItem("tieCount", ties);
+  sessionStorage.setItem("lossCount", losses);
+  document.getElementById('wins').innerHTML = sessionStorage.getItem("winCount");
+  document.getElementById('ties').innerHTML = sessionStorage.getItem("tieCount");
+  document.getElementById('losses').innerHTML = sessionStorage.getItem("lossCount");
+}, 200);
+
+function reset() {
+  sessionStorage.setItem("winCount", 0);
+  sessionStorage.setItem("tieCount", 0);
+  sessionStorage.setItem("lossCount", 0);
+  document.getElementById('wins').innerHTML = sessionStorage.getItem("winCount");
+  document.getElementById('ties').innerHTML = sessionStorage.getItem("tieCount");
+  document.getElementById('losses').innerHTML = sessionStorage.getItem("lossCount");
+  document.getElementById('o-img').src = "svg/mystery.svg";
+  document.getElementById('result').innerHTML = "Result";
 }
